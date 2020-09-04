f33544fd2e441388fadc5f4c359394a37453254a
Troels Agergaard <TroelsAgergaard@users.noreply.github.com>
2020-09-04 13:47:47 +0200

    Add large jpg 04

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import pngimg from './media/largepng.png';
+import jpgimg from './media/pgm004.jpg';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={pngimg} alt="png" />
+				<img src={jpgimg} alt="png" />
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
